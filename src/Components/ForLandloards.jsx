65ffdc85dab6a6ed5342404c8e37ea5778359229
Errytagedesign <errytage@gmail.com>
2023-11-18 21:40:06 +0100

    Update tab contents

--- src/Components/ForLandloards.jsx
+++ src/Components/ForLandloards.jsx
@@ -4,11 +4,11 @@ import Button from './Button/Button';
 function ForLandloards() {
   return (
     <main>
-      <h2 className='my-4'>Be the first to know when we go live! ttt</h2>
+      <h2 className='my-4'>Be the first to know when we go live!</h2>
       <p>
-        Whether it’s selling your current home, getting financing, renting or
-        buying a new home, we make it easy and efficient. The best part? you’ll
-        save a bunch of money and time with our services.
+        Whether it&apos;s selling your current home, getting financing, renting
+        or buying a new home, we make it easy and efficient. The best part?
+        you&apos;ll save a bunch of money and time with our services.
       </p>
       <Button title='JOIN OUR WAIT  LIST' url='/join-form' />
     </main>
--- src/Components/ForTenants.jsx
+++ src/Components/ForTenants.jsx
@@ -6,9 +6,10 @@ function ForTenants() {
     <main>
       <h2 className='my-4'>Be the first to know when we go live!</h2>
       <p>
-        Whether it’s selling your current home, getting financing, renting or
-        buying a new home, we make it easy and efficient. The best part? you’ll
-        save a bunch of money and time with our services.
+        Are you seeking affordable rental options? Need a workspace or
+        short-term accommodation? At Ile - Mi, we've got you covered, making
+        your search easy and efficient. The best part? you&apos;ll save a bunch
+        of money and time with our services.
       </p>
 
       <Button title='JOIN OUR WAIT  LIST' url='/join-form' />
